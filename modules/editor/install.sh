8930bc27b424cd80af80b86b50a126436ffe30b2
Yrashka <djliana2020@gmail.com>
2026-08-10 18:36:48 +0300

    Refactor editor install script for alias and permissions
    Updated the install script to use 'main.sh' for ide alias and added chmod for simple_editor.sh.

--- modules/editor/install.sh
+++ modules/editor/install.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 EDITOR_TYPE="$1"
 case $EDITOR_TYPE in
-helix)
-  pkg install -y helix
-  mkdir -p ~/.config/helix
-  cp modules/editor/config/helix/config.toml ~/.config/helix/
-  echo "alias hx=helix" >>~/.bashrc
-  echo "alias ide='bash core/init.sh start'" >>~/.bashrc
-  ;;
-micro)
-  pkg install -y micro
-  echo "alias micro='micro -colorscheme solarized'" >>~/.bashrc
-  echo "alias ide='micro'" >>~/.bashrc
-  ;;
-neovim)
-  pkg install -y neovim
-  echo "alias vim=nvim" >>~/.bashrc
-  echo "alias ide='nvim'" >>~/.bashrc
-  ;;
-*)
-  pkg install -y helix
-  mkdir -p ~/.config/helix
-  cp modules/editor/config/helix/config.toml ~/.config/helix/
-  echo "alias hx=helix" >>~/.bashrc
-  echo "alias ide='bash core/init.sh start'" >>~/.bashrc
-  ;;
+    helix)
+        pkg install -y helix
+        mkdir -p ~/.config/helix
+        cp modules/editor/config/helix/config.toml ~/.config/helix/
+        echo "alias hx=helix" >> ~/.bashrc
+        echo "alias ide='bash main.sh'" >> ~/.bashrc
+        ;;
+    micro)
+        pkg install -y micro
+        echo "alias micro='micro -colorscheme solarized'" >> ~/.bashrc
+        echo "alias ide='bash main.sh'" >> ~/.bashrc
+        ;;
+    neovim)
+        pkg install -y neovim
+        echo "alias vim=nvim" >> ~/.bashrc
+        echo "alias ide='bash main.sh'" >> ~/.bashrc
+        ;;
+    *)
+        pkg install -y helix
+        mkdir -p ~/.config/helix
+        cp modules/editor/config/helix/config.toml ~/.config/helix/
+        echo "alias hx=helix" >> ~/.bashrc
+        echo "alias ide='bash main.sh'" >> ~/.bashrc
+        ;;
 esac
+chmod +x modules/editor/simple_editor.sh
+echo "alias sedit='bash modules/editor/simple_editor.sh edit'" >> ~/.bashrc
+echo "alias sopen='bash modules/editor/simple_editor.sh open'" >> ~/.bashrc
 echo -e "${GREEN}Editor installed: $EDITOR_TYPE${NC}"
+echo -e "${GREEN}Simple Editor commands:${NC}"
+echo "  sedit <file>  - Edit/create file"
+echo "  sopen <file>  - Open and view file"
